46a88e3063cb1f811e12208a3fe6f497cdb53ee1
David Miller <dmiller2606@gmail.com>
2023-12-06 16:11:03 +0100

    New Day05 Solution using Range math

--- 2023/05.py
+++ 2023/05.py
@@ -1,49 +1,63 @@
-from itertools import chain, filterfalse, pairwise
+from itertools import filterfalse, pairwise
 from icecream import ic
 import re
 import time
 
 def adjustOldMappings(newMappings, oldMaps):
-    newMappingsAdjusted = list()
+    newMappingsAdjusted = [(oldMap[0],oldMap[1],'Old') for oldMap in oldMaps]
     for newMapping in newMappings:
-        if len(oldMaps) == 0:
-            newMappingsAdjusted.append(newMapping)
-            continue
         newStart, newEnd = newMapping[0]
-        adjusted = False
+        newStartAdjusted,newEndAdjusted = newMapping[0]
         for oldMapping in oldMaps:
             oldStart, oldEnd = oldMapping[0]
+            oldStart += oldMapping[1]
+            oldEnd += oldMapping[1]
             #Adjust Old Mappings with new Values if range is somewhere in there
-            if oldMapping[0][0] + oldMapping[1] <= newMapping[0][0] and oldMapping[0][1] + oldMapping[1] >= newMapping[0][1]:
-                oldMappingPL = ((oldMapping[0][0],newMapping[0][0]-oldMapping[1]),oldMapping[1], 'MidLeft')
-                oldMappingMid = ((newMapping[0][0]-oldMapping[1],newMapping[0][1]-oldMapping[1]),oldMapping[1] + newMapping[1],'Mid')
-                oldMappingPR = ((newMapping[0][1]-oldMapping[1],oldMapping[0][1]),oldMapping[1], 'MidRight')
+            if oldStart <= newStart and oldEnd >= newEnd:
+                oldMappingPL = ((oldMapping[0][0],newStart-oldMapping[1]),oldMapping[1], 'Old')
+                oldMappingMid = ((newStart-oldMapping[1],newEnd-oldMapping[1]),oldMapping[1] + newMapping[1],'')
+                oldMappingPR = ((newEnd-oldMapping[1],oldMapping[0][1]),oldMapping[1], 'Old')
                 temp = [oldMappingPL,oldMappingMid,oldMappingPR]
                 for mapping in temp:
                     if not mapping[0][0] + 1 >= mapping[0][1]:
                         newMappingsAdjusted.append(mapping)
-            elif newMapping[0][0] <= oldMapping[0][0] + oldMapping[1] and oldMapping[0][1] + oldMapping[1] <= newMapping[0][1]:
-                oldMappingAdjusted = (oldMapping[0],oldMapping[1] + newMapping[1],'Complete Overlap')
+            elif newStart <= oldStart and oldEnd <= newEnd:
+                oldMappingAdjusted = (oldMapping[0],oldMapping[1] + newMapping[1],'')
                 newMappingsAdjusted.append(oldMappingAdjusted)
-            elif oldMapping[0][0] + oldMapping[1] < newMapping[0][1] <= oldMapping[0][1] + oldMapping[1]:
-                oldMappingPL = ((oldMapping[0][0],newMapping[0][1]-oldMapping[1]),oldMapping[1]+newMapping[1],'LeftNew')
-                oldMappingPR = ((newMapping[0][1]-oldMapping[1],oldMapping[0][1]),oldMapping[1], 'LeftOld')
+            elif oldStart < newEnd <= oldEnd:
+                oldMappingPL = ((oldMapping[0][0],newEnd-oldMapping[1]),oldMapping[1]+newMapping[1],'')
+                oldMappingPR = ((newEnd-oldMapping[1],oldMapping[0][1]),oldMapping[1], 'Old')
                 newMappingsAdjusted.append(oldMappingPL)
                 newMappingsAdjusted.append(oldMappingPR)
-            elif oldMapping[0][1] + oldMapping[1] > newMapping[0][0] >= oldMapping[0][0] + oldMapping[1]:
-                oldMappingPL = ((oldMapping[0][0],newMapping[0][0]-oldMapping[1]),oldMapping[1], 'RightOld')
-                oldMappingPR = ((newMapping[0][0]-oldMapping[1],oldMapping[0][1]),oldMapping[1]+newMapping[1],'RightNew')
+            elif oldEnd > newStart >= oldStart:
+                oldMappingPL = ((oldMapping[0][0],newStart-oldMapping[1]),oldMapping[1], 'Old')
+                oldMappingPR = ((newStart-oldMapping[1],oldMapping[0][1]),oldMapping[1]+newMapping[1],'')
                 newMappingsAdjusted.append(oldMappingPL)
                 newMappingsAdjusted.append(oldMappingPR)
             #Adjust new value of Range so it doesn't overlap with older ranges
-            if oldMapping[0][0] <= newMapping[0][1] < oldMapping[0][1]:
-                newEnd = oldMapping[0][0]
-            if oldMapping[0][0] < newMapping[0][0] <= oldMapping[0][1]:
-                newStart = oldMapping[0][1]
-        if not newStart >= newEnd:
-            newMappingsAdjusted.append(((newStart,newEnd),newMapping[1]))
-    ic(newMappingsAdjusted)
+            if oldMapping[0][0] <= newEnd < oldMapping[0][1]:
+                newEndAdjusted = oldMapping[0][0]
+            if oldMapping[0][0] < newStart <= oldMapping[0][1]:
+                newStartAdjusted = oldMapping[0][1]
+        if not newStartAdjusted >= newEndAdjusted:
+            newMappingsAdjusted.append(((newStartAdjusted,newEndAdjusted),newMapping[1],''))
+    newMappingsAdjusted.sort(key=lambda x: x[0][0])
     return newMappingsAdjusted
+
+def collapseOverlap(maps):
+    filtered_data = {}
+    for item in maps:
+        key = item[0][0]
+        end = item[0][1]
+        if key in filtered_data:
+            if item[2] == 'Old' or filtered_data[key][0][1] < end:
+                if filtered_data[key][2] == 'Old' and end < filtered_data[key][0][1]:
+                    filtered_data[key] = item
+                continue
+        filtered_data[key] = item
+
+    result = list(filtered_data.values())
+    return result
 
 def mappingGenerator(maps):
     mappings = list()
@@ -52,42 +66,45 @@ def mappingGenerator(maps):
     for line in maps:
         if line == '':
             mappings = adjustOldMappings(newMappings,mappings)
+            #ic(mappings)
+            mappings = collapseOverlap(mappings)
             newMappings = list()
             continue
         numbers = [int(n) for n in line.split(' ')]
         mapping = ((numbers[1],numbers[1] + numbers[2]),numbers[0]-numbers[1])
         newMappings.append(mapping)
+    mappings = adjustOldMappings(newMappings,mappings)
+    mappings = collapseOverlap(mappings)
+    #ic(mappings)
+    mappings = [(mapping[0],mapping[1]) for mapping in mappings]
+    return mappings
 
-def evaluateMap(line: str,seed: tuple[int,bool]):
-    mapping = [int(n) for n in line.split(' ')]
-    if not mapping[1] <= seed[0] < mapping[1]+mapping[-1] or seed[1]:
-        return seed
-    converted = seed[0] - (mapping[1] - mapping[0])
-    return converted,True
-
-def mapSeedsToLocations(seeds, maps):
-    for line in maps:
-        if any(c.isdigit() for c in line):
-            seeds = list(map(lambda x: evaluateMap(line,x),seeds))
-        if line == '':
-            seeds = [(seed[0],False) for seed in seeds]
-    return [seed[0] for seed in seeds]
-
-def solutionB(input):
-    seeds = [int(n) for n in re.findall(r'\d+',input[0])]
-    seeds = list(pairwise(seeds))[0::2]
-    seeds = [list(range(seedRange[0],seedRange[0]+seedRange[1])) for seedRange in seeds]
-    seeds = [(n,False) for n in list(chain.from_iterable(seeds))]
-    locations = mapSeedsToLocations(seeds,input[2:])
-    print(min(locations))
-        
-def solutionA(input):
-    seeds = [(int(n),False) for n in re.findall(r'\d+',input[0])]
-    locations = mapSeedsToLocations(seeds,input[2:])
-    print(min(locations))
+def findMinLocation(seedRanges,lowestMappings):
+    for range1 in lowestMappings:
+        for range2 in seedRanges:
+            intersection_start = max(range1[0][0][0], range2[0])
+            intersection_end = min(range1[0][0][1], range2[1])
+            if intersection_start <= intersection_end:
+                intersection_min = min(range(intersection_start, intersection_end + 1))
+                return intersection_min + range1[0][1]
+    return -1
 
 def newSolutionA(input):
+    seeds = [int(n) for n in re.findall(r'\d+',input[0])]
     mappings = mappingGenerator(input[2:])
+    seedLocations = [seed + mapping[1] for seed in seeds for mapping in mappings if seed in range(mapping[0][0],mapping[0][1])]
+    #ic(seedLocations)
+    ic(min(seedLocations))
+
+def newSolutionB(input):
+    mappings = mappingGenerator(input[2:])
+    seeds = [int(n) for n in re.findall(r'\d+',input[0])]
+    seedRanges = list(pairwise(seeds))[0::2]
+    seedRanges = [(n[0],n[0]+n[1]) for n in seedRanges]
+    lowestMappings = [(mapping,mapping[0][0] + mapping[1]) for mapping in mappings]
+    lowestMappings.sort(key=lambda x: x[1])
+    min_location = findMinLocation(seedRanges,lowestMappings)
+    ic(min_location)
     
 def parseInput(filepath: str):
     with open(filepath) as file:
@@ -96,10 +113,9 @@ def parseInput(filepath: str):
     return lines
 
 def main():
-    input = parseInput(r'2023\testinput05.txt')
-    #solutionA(input)
-    #solutionB(input)
+    input = parseInput(r'2023\input05.txt')
     newSolutionA(input)
-
+    newSolutionB(input)
+    
 if __name__ == '__main__':
     main()
